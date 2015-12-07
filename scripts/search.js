4726ea5b65f9755f8e41b438e5e325da83785cda
ejdysinger@gmail.com <ejdysinger@gmail.com>
2015-12-07 15:53:42 -0700

    pulling

--- scripts/search.js
+++ scripts/search.js
@@ -4,3 +4,31 @@ function Search() {
 function insertBranch(branch_id, street, postal, city, manager_id){
 	console.log("here")
 }
+
+function insertEmployee(emp_id, emp_class, lname, fname, branch_id, sex, phone_num, age){
+	
+}
+
+function insertClient(client_id, lname, fname, sex, miles_driven, branch_id, valid_license){
+	
+}
+
+function insertVehicle(vehicle_id, milage, emp_id){
+	
+}
+
+function insertLesson(lesson_id, date, client_id, emp_id, vehicle_id, mileage){
+	
+}
+
+function insertVI(vehicle_id, date, fault){
+	
+}
+
+function insertExam(exam_id, type, client_id, result){
+	
+}
+
+function ionsertInterview(client_id, emp_id, description){
+	
+}
