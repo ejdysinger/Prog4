62dc7dc1c584de50ba415c25178cd8ac9bbf6240
tkoch <tkoch@email.arizona.edu>
2015-12-07 16:47:17 -0700

    javascript and modified forms

--- scripts/search.js
+++ scripts/search.js
@@ -4,3 +4,12 @@ function Search() {
 function insertBranch(branch_id, street, postal, city, manager_id){
 	console.log("here")
 }
+function testResults (form) {
+    var branch_id = form.branch_id.value;
+	var street = form.street.value;
+    var postal = form.postal.value;
+    var city = form.city.value;
+    var manager_id = form.manager_id.value;
+
+    alert ("You typed: " + branch_id );
+}
